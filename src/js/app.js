82a062730bf923d17581be341baa05883a6165b1
Shu Uesugi <shu@chibicode.com>
2016-07-04 04:23:48 -0700

    Article 3

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
+import './vendor/jquery.fitvids.js'
 import $ from 'jquery'
 window.$ = $
 
 $(() => {
   $('a[href^="http"]').attr('target','_blank')
+  $('.js-post-content').fitVids({ customSelector: 'iframe[src*="slideshare.net"]' })
 })
