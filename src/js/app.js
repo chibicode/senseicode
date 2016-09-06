d9ab5d795f019928e170777a7061b8feea47296b
Shu Uesugi <shu@chibicode.com>
2016-09-06 01:40:30 -0700

    Responsive embed TED

--- src/js/app.js
+++ src/js/app.js
@@ -4,5 +4,5 @@ window.$ = $
 
 $(() => {
   $('a[href^="http"]').attr('target','_blank')
-  $('.js-post-content').fitVids({ customSelector: 'iframe[src*="slideshare.net"]' })
+  $('.js-post-content').fitVids({ customSelector: 'iframe[src*="slideshare.net"], iframe[src*="ted.com"]' })
 })
